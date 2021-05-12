3c5b12407e90da2817118c04a43511b77fe2dedc
gaurav bhardwaj <myselfgroot@gmail.com>
2021-05-12 16:50:44 +0530

    added

--- src/com/company/Todos.java
+++ src/com/company/Todos.java
@@ -11,15 +11,12 @@ public class Todos extends JPanel {
     }
 
     void init() {
-//        setSize(500, 400);
+        setPreferredSize(new Dimension(450, 400));
         setLayout(new FlowLayout(FlowLayout.LEFT));
     }
 
     void buildUI() {
         title = new JLabel("Todos:");
-        title.setPreferredSize(new Dimension(400, 20));
-        title.setOpaque(true);
-        title.setBackground(Color.blue);
         add(title);
     }
 }
