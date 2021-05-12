96e755fc14ed8f985b03b1dcb0faaf17cb97fc9a
gaurav bhardwaj <myselfgroot@gmail.com>
2021-05-12 13:02:31 +0530

    added

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -5,13 +5,9 @@ import java.awt.*;
 
 public class App extends JFrame {
     final int WIDTH = 400;
-    final int HEIGHT = 400;
+    final int HEIGHT = 600;
 
     final Dimension dimensions = new Dimension(WIDTH, HEIGHT);
-
-    JButton btn;
-    JTextField field;
-    JLabel label;
 
     App() {
         super("First love my bora 4");
@@ -23,24 +19,13 @@ public class App extends JFrame {
 
     void init() {
         setLayout(new FlowLayout(FlowLayout.CENTER));
-//        initial code
         setSize(dimensions);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
     }
 
     void buildUI() {
-        btn = new JButton("Button 1");
-        label = new JLabel("label");
-        field = new JTextField(22);
-        add(field);
-        add(btn);
-        add(label);
 
-
-        btn.addActionListener((e) ->{
-            label.setText(field.getText());
-        });
     }
 
 }
